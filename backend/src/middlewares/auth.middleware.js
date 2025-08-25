7522df58c92973f011dd92ecd5c24db8f10bff0c
BitWiseNexus <adityarehpade1@gmail.com>
2025-08-25 16:12:17 +0530

    Updated Landind Page

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,42 @@
+import jwt from 'jsonwebtoken'
+import { config } from 'dotenv'
+import User from '../models/user.model.js';
+
+config();
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.json({ success: false, message: 'Not Authorized. Login Again.' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ success: false, message: 'Not Authorized. Login Again.' });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        req.userId = user._id;
+        req.user = user;
+        next();
+        
+    } catch (error) {
+        console.log("Error in protectRoute middleware: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ success: false, message: "Access denied" });
+    }
+    next();
+  };
+};
